c632220c660a2beca25fbc63f4864098f47c5864
ipioselli <ines.pioselli@gmail.com>
2022-04-07 14:46:01 -0400

    final: added sale page

--- Class 04/Assignment 01/Final Website/js/script.js
+++ Class 04/Assignment 01/Final Website/js/script.js
@@ -1,4 +1,11 @@
+
+
 //fades in the secret document when the button is clicked
 $(`#button`).on(`click`, function(event){
   $(`#secret-document`).fadeIn();
+});
+
+//fades in the secret document when the button is clicked
+$(`#sale-button`).on(`click`, function(event){
+  $(`#secret-info`).fadeIn();
 });
