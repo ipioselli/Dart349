e06ee3209b3de2b3da20e023b64302de6a557f7c
ipioselli <ines.pioselli@gmail.com>
2022-04-07 23:34:57 -0400

    final:done

---
+++ Class 04/Assignment 01/Artist-Statement/Final Website/js/script.js
@@ -0,0 +1,30 @@
+//Main Javascript Code for button fade in
+
+//fades in the secret document when the button is clicked
+$(`#button`).on(`click`, function(event){
+  $(`#secret-document`).fadeIn();
+});
+
+
+$(`#sale-button`).on(`click`, function(event){
+  $(`#secret-info`).fadeIn();
+});
+
+
+//fade in buttons for men and women section
+
+$(`#show-button`).on(`click`, function(event){
+  $(`#secret-women`).fadeIn();
+});
+
+$(`#show-button`).on(`click`, function(event){
+  $(`#secret-men`).fadeIn();
+});
+
+$(`#button-01`).on(`click`, function(event){
+  $(`#second-box`).fadeIn();
+});
+
+$(`#button-02`).on(`click`, function(event){
+  $(`#third-box`).fadeIn();
+});
